ec36d1c67640de9f7badaaa0816b86c55e4a75c2
ak101ak101 <akshatdot@gmail.com>
2022-09-11 17:10:45 +0530

    for testing

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,10 @@
+
+
+import 'dart:ffi';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:newbie/screens/signin_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:newbie/screens/chartbar.dart';
@@ -10,7 +16,9 @@ class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
-final List<transaction> transa = [];
+
+ List<transaction> transa = [];
+
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
@@ -18,7 +26,8 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
 
         appBar: AppBar(title: Text("Expense Calculator")),
-        body: SingleChildScrollView(child: Column(crossAxisAlignment: CrossAxisAlignment.stretch,
+        body:Column(children:<Widget>[
+            SingleChildScrollView(child: Column(crossAxisAlignment: CrossAxisAlignment.stretch,
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 // ignore: prefer_const_literals_to_create_immutables
                 children: <Widget>[
@@ -33,6 +42,10 @@ class _HomeScreenState extends State<HomeScreen> {
 
 
     ),
+          Expanded(
+         child: UserInformation(),),
+        ]
+        ),
 
 bottomNavigationBar:     Container(
   padding:const EdgeInsets.all(10),
@@ -94,10 +107,17 @@ class _TransactionListState extends State<TransactionList> {
     });
   }
 
-  void addnewtx(String txtitle, double amont, DateTime chosendate) {
+  Future<void> addnewtx(String txtitle, double amont, DateTime chosendate) async {
     final newtx =
     transaction(title: txtitle, amt: amont, id: c, date: chosendate);
-
+    final User? user = FirebaseAuth.instance.currentUser;
+    final uid = user?.uid;
+    DocumentReference<Map<String, dynamic>> users = FirebaseFirestore.instance.collection("users").doc(uid).collection("items").doc();
+    users.set({
+      'title':txtitle,
+      'amount': amont,
+      'Date':chosendate.toString(),
+    });
     setState(() {
       transa.add(newtx);
       c++;
@@ -180,34 +200,37 @@ class _TransactionListState extends State<TransactionList> {
                       ),
 
                       Column(
-                        children: transa.map((tx) {
-                          return Card(
-                            elevation: 5,
-                            margin: EdgeInsets.symmetric(
-                                vertical: 8, horizontal: 5),
-                            child: ListTile(
-                              leading: CircleAvatar(
-                                  radius: 30,
-                                  child:
-                                  FittedBox(child: Text(tx.id.toString()))),
-                              title: Text(
-                                  tx.title +
-                                      " \n" +
-                                      "Cost=" +
-                                      tx.amt.toString(),
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20,
-                                      color: Colors.purple)),
-                              subtitle:
-                              Text(DateFormat('EEE,d/M/y').format(tx.date)),
-                              trailing: IconButton(
-                                  icon: Icon(Icons.delete),
-                                  onPressed: () =>
-                                      _deletetrans(tx.id.toString())),
-                            ),
-                          );
-                        }).toList(),
+                        children:<Widget> [
+
+                        ]
+                        // children: transa.map((tx) {
+                        //   return Card(
+                        //     elevation: 5,
+                        //     margin: EdgeInsets.symmetric(
+                        //         vertical: 8, horizontal: 5),
+                        //     child: ListTile(
+                        //       leading: CircleAvatar(
+                        //           radius: 30,
+                        //           child:
+                        //           FittedBox(child: Text(tx.id.toString()))),
+                        //       title: Text(
+                        //           tx.title +
+                        //               " \n" +
+                        //               "Cost=" +
+                        //               tx.amt.toString(),
+                        //           style: TextStyle(
+                        //               fontWeight: FontWeight.bold,
+                        //               fontSize: 20,
+                        //               color: Colors.purple)),
+                        //       subtitle:
+                        //       Text(DateFormat('EEE,d/M/y').format(tx.date)),
+                        //       trailing: IconButton(
+                        //           icon: Icon(Icons.delete),
+                        //           onPressed: () =>
+                        //               _deletetrans(tx.id.toString())),
+                        //     ),
+                        //   );
+                        // }).toList(),
 
                       ),
 
@@ -297,3 +320,80 @@ class _ChartState extends State<Chart> {
 //     );
 //   }
 // }
+
+final User? user = FirebaseAuth.instance.currentUser;
+final uid = user?.uid;
+class UserInformation extends StatefulWidget {
+  const UserInformation({super.key});
+
+  @override
+  _UserInformationState createState() => _UserInformationState();
+}
+
+
+class _UserInformationState extends State<UserInformation> {
+  @override
+  initState() {
+    super.initState();
+  }
+
+   Stream<QuerySnapshot> itemslist = FirebaseFirestore.instance.collection("users").doc(uid).collection("items").snapshots();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: itemslist,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot)
+    {
+      if (snapshot.hasError) {
+        return Text('Something went wrong');
+      }
+
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return Text("Loading");
+      }
+      // snapshot.data!.docs.map()forEach(data)=>(
+      // transa.add(transaction(title:data["title"]?? "",amt:data["amount"]?? 0.0,date:DateTime.parse(data["Date"])??DateTime.now(),id:2));
+      // );
+      print("hellllooo");
+      print(snapshot.data!.docs.first["title"]);
+      snapshot.data!.docs.forEach((element) { });
+      transa = snapshot.data!.docs.map((ata)=>transaction(title:ata["title"]
+          ?? "",amt:ata["amount"]?? 0.0,date:DateTime.parse(ata["Date"])??DateTime.now(),id:2)).toList();
+      return ListView(
+        children: snapshot.data!.docs.map((DocumentSnapshot document) {
+          Map<String, dynamic> data = document.data()! as Map<String,
+              dynamic>;
+
+          print(data["title"]);
+          data.forEach((key, value) { });
+          return ListTile(
+              leading: CircleAvatar(
+                  radius: 30,
+                  child:
+                  FittedBox(child: Text("1"))),
+              title: Text(
+                  data["title"] +
+                      " \n" +
+                      "Cost=" +
+                      data["amount"].toString(),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple)),
+              subtitle:
+              Text(data["Date"]),
+
+              trailing: IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () => {}
+                // _deletetrans(tx.id.toString())),
+              )
+          );
+        }
+        ).toList(),
+      );
+        },
+    );
+  }
+}
